fc36a206c34acdffd736a0e997a9c8286845f26b
Fernando <fernandoaju78@gmail.com>
2021-10-21 22:53:38 -0300

    a11 update
    Change-Id: I4f582b45c9d47b0fe36c525483e2050631d2893d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_cheeseburger_dumpling.mk
+    $(LOCAL_DIR)/twrp_cheeseburger.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_cheeseburger-eng \
+    twrp_cheeseburger-userdebug
