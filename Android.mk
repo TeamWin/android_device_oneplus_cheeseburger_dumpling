eb480f7a4886967e79f2acbdcb63aca167610b61
Fernando <fernandoaju78@gmail.com>
2021-10-21 22:53:38 -0300

    Oneplus 5: a11 update
    Change-Id: I4f582b45c9d47b0fe36c525483e2050631d2893d

--- Android.mk
+++ Android.mk
@@ -25,7 +25,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),cheeseburger_dumpling)
+ifeq ($(TARGET_DEVICE),cheeseburger)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 endif
