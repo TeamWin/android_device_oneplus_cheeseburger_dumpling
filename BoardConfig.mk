287581dd2fae8c729b9a2104370b982b7f803069
CaptainThrowback <captainthrowback@hotmail.com>
2023-09-21 16:17:28 -0300

    op5_t: Boardconfig clean - libxml2 is built by default
    Change-Id: I9a28d77f17d0075625315114ee97e280dccac15b
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,13 +147,11 @@ endif
 
 TARGET_RECOVERY_DEVICE_MODULES += \
 	libion \
-	libxml2 \
 	vendor.display.config@1.0 \
 	vendor.display.config@2.0
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
 
