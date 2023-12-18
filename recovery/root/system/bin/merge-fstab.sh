7eb9e1f5e177185c6bcc457b6c7572b55e4ff823
Fernando Oliveira <fernandoaju78@gmail.com>
2023-12-18 18:55:03 -0300

    op5_t: make the dyn build universal again
    Change-Id: I12b1efce9cb6519560c361b9c50b54fd92d1489d
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

---
+++ recovery/root/system/bin/merge-fstab.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+# Dynamic Partitions
+if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep -qE 'cheesedump_dynpart|cheeseburger_dynpart' > /dev/null; then
+    echo >> /system/etc/recovery.fstab
+    for p in system system_ext product vendor odm; do
+        echo "${p} /${p} ext4 ro,barrier=1,discard wait,logical" >> /system/etc/recovery.fstab
+    done
+    echo >> /system/etc/twrp.flags
+    for p in vendor; do
+        echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
+    done
+else
+    echo >> /system/etc/twrp.flags
+    cat /fstab/twrp.flags.nondynpart >> /system/etc/twrp.flags
+    echo "/super emmc /dev/block/by-name/system flags=display=\"Super\";backup=1" >> /system/etc/twrp.flags
+    for p in vendor; do
+        echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
+    done
+fi
